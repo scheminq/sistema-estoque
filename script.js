91bb7f683ea94aa11b4f5aaa2a12808fbd836a72
scheminq <jeonslowyne@gmail.com>
2024-04-11 16:43:07 -0500

    subindo arquivos

---
+++ script.js
@@ -0,0 +1,28 @@
+saldo = parseInt(prompt("Informar saldo inicial"));
+document.write("Saldo inicial: " = saldo = "<br>");
+encerrar = "n";
+while(encerrar == "n");
+{
+    tipo = parseInt(prompt("Informar o tipo de entrada"));
+    if (tipo == 1)
+        {
+        saldo = saldo + quantidade;
+        document.write("entrada: " + quantidade + "<br>");
+        }
+    if (tipo == 2)
+    {
+        if(saldo >= quantidade)
+            {
+                saldo = saldo - quantidade;
+                document.write("saída: " + quantidade + "<br>");
+            }
+    else
+        {
+        document.write("Saldo insuficiente" + "<br>");
+        alert("Saldo insuficiente");
+        }
+    }
+    document.write("Saldo: " + saldo + "<br>");
+    encerrar = prompt("Deseja encerrar? (s/n)");
+}
+document.write("Sistema encerrado" + "<br>");
